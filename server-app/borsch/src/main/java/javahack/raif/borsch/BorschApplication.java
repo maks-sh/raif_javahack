e318dfc3756cdb90016bc6984747ff9797e99406
DenRUS <denrus1995@gmail.com>
2019-09-14 15:18:06 +0300

    server init

---
+++ server-app/borsch/src/main/java/javahack/raif/borsch/BorschApplication.java
@@ -0,0 +1,13 @@
+package javahack.raif.borsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorschApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorschApplication.class, args);
+    }
+
+}
---
+++ server-app/borsch/src/main/java/javahack/raif/borsch/controllers/TestController.java
@@ -0,0 +1,24 @@
+package javahack.raif.borsch.controllers;
+
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Тестовое апи.
+ */
+@RestController
+@RequestMapping("/test/")
+public class TestController {
+
+    private static final Logger ROOT_LOGGER = LogManager.getRootLogger();
+    private static final Logger BORSCH_LOGGER = LogManager.getLogger("borsch.logger");
+    @GetMapping("heartbeat")
+    public String areYouAlive() {
+        ROOT_LOGGER.info("Лог только в консоль");
+        BORSCH_LOGGER.info("Лог и в консоль, и в файл");
+        return "I`m ok";
+    }
+}
